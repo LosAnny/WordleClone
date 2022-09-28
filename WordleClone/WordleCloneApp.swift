7e4b4bbb36194b3a2fc48b14d6f2c84009139784
los.anny <ann.losh99@mail.ru>
2022-09-28 13:00:42 +0300

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Анна Лошакова on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
